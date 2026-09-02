749113ac41b5b913e8f80177c4f8ae6300a7a015
boxsteerr-pixel <boxsteerr@gmail.com>
2026-09-02 20:56:17 +0800

    Refresh cache for child-friendly controls

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'english-recorder-pwa-v4';
+const CACHE_NAME = 'english-recorder-pwa-v5';
 const SHELL = ['./', './index.html', './app.js', './manifest.webmanifest', './icons/icon.svg'];
 
 self.addEventListener('install', (event) => {
